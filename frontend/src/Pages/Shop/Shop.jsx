97f551bd356c9909af1049cbead395eb05c707f6
shainyachary <shainy.achary@gmail.com>
2024-05-03 16:34:43 +0530

    initial commit

--- frontend/src/Pages/Shop/Shop.jsx
+++ frontend/src/Pages/Shop/Shop.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import "./Shop.css";
 import { ProductContext } from "../../ProductContext/ProductContext";
 import Product from "../../Components/Product/Product";
-import { data } from "../../utils";
 
 const Shop = () => {
   const { products } = useContext(ProductContext);
@@ -11,17 +10,6 @@ const Shop = () => {
     <section className="shop_container">
       <h1 className="title mt-5">Latest Collections</h1>
       <div className="product_wrapper mt-4">
-        {data?.map((d) => {
-          return (
-            <Product
-              key={d.id}
-              id={d.id}
-              name={d.name}
-              image={d.image}
-              price={d.price}
-            />
-          );
-        })}
         {products?.data?.data?.map((prod) => {
           return <Product key={prod.id} {...prod} />;
         })}
